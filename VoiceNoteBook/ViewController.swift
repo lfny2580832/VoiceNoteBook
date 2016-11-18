0ca11b7f4a46f928a73d10f0b8e15a27f9cc3076
niuyan <632176758@qq.com>
2016-11-18 10:13:34 +0800

    s

---
+++ VoiceNoteBook/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  VoiceNoteBook
+//
+//  Created by 牛严 on 2016/11/18.
+//  Copyright © 2016年 牛严. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

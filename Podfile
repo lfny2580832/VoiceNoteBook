63f18981a7dbcd499142777d21ba72df24b7a583
niuyan <632176758@qq.com>
2016-11-19 21:00:14 +0800

    使用cocoapods集成七牛sdk

---
+++ Podfile
@@ -0,0 +1,10 @@
+source 'https://github.com/CocoaPods/Specs.git'
+platform :ios, ’10.0’
+
+use_frameworks!
+
+target “VoiceNoteBook” do
+
+pod "Qiniu", "~> 7.1"
+
+end
---
+++ VoiceNoteBook/BridgeHeader.h
@@ -0,0 +1,14 @@
+//
+//  BridgeHeader.h
+//  VoiceNoteBook
+//
+//  Created by 牛严 on 2016/11/19.
+//  Copyright © 2016年 牛严. All rights reserved.
+//
+
+#ifndef BridgeHeader_h
+#define BridgeHeader_h
+
+
+
+#endif /* BridgeHeader_h */
